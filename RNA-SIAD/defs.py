1e1e991f67f3cfb0893dfd52c416bbae66d9f0b1
Hebert Campos <90910343+HebertCampos@users.noreply.github.com>
2022-06-22 21:10:56 -0300

    Add files via upload

---
+++ RNA-SIAD/defs.py
@@ -0,0 +1,40 @@
+import random, math
+
+def peso():
+    peso = []
+    for i in range(3):
+        p =[]
+        for j in range(2):
+            p.append(random.randint(-2,2))
+        peso.append(p)
+    return peso
+
+def biass():
+    bias = []
+    b = []
+    for i in range(2):
+        b.append(random.randint(-2,2))
+    bias.append(b)
+    b = [random.randint(-2,2)]
+    bias.append(b)
+    return bias
+
+# somatorio = m2[i]*peso[0][j]+cond[i]*peso[1][j]
+def somatori0(norm_m2, norm_cond, peso):
+    som = []
+    for i in range(len(norm_m2)):
+        s = 0
+        s += norm_m2[i]*peso[0]+norm_cond[i]*peso[1]
+        som.append(s)
+    return som
+
+def saidaN(somatorio, bias):
+    #ativação 1/(1+math.exp(somatorio[j]+bias[i]))
+    atv =[]
+    for j in range(len(somatorio)):
+        ns = []
+        for i in range(len(bias)):
+            n = 1/(1+math.exp(-(somatorio[j][i]+bias[i])))
+            ns.append(n)
+        atv.append(ns)
+    return atv
---
+++ RNA-SIAD/main.py
@@ -0,0 +1,60 @@
+from defs import *
+
+# x1 = [140,130,90,75,100]
+# x2 = [500,600,400,450,800]
+# v_esperado = [700000,650000,450000,330000,675000]
+x1 = [78,85,97,280,42,282,73]
+x2 = [550,550,1300,1800,42,1800,550]
+v_esperado = [300000, 545244,45000,5200000,35000,1550000,395000]
+
+epocas = 10000
+tx_aprend = 0.005
+
+# normaliza entradas entre 0-1
+norm_x1 = [(x-min(x1))/(max(x1)-min(x1)) for x in x1]
+norm_x2 = [(x-min(x2))/(max(x2)-min(x2)) for x in x2]
+norm_v_esperado = [(x-min(v_esperado))/(max(v_esperado)-min(v_esperado)) for x in v_esperado]
+
+pesos = peso()
+bias = biass()
+
+for q in range(epocas):
+
+    # calcula o somatorio = (x1*peso[0][0]+x2*peso[0][1]+...+xn*peso[0][n])
+    somatoria = [somatori0(norm_x1, norm_x2, pesos[j]) for j in range(2)]
+    somatoria_N1_N2 = list(zip(somatoria[0], somatoria[1]))
+    # v_esperado do Neuronio 1 e 2 - usando ativação segmoid
+    # N1 =1/1+e**(somatoria_N1_N2 + bias[0][0])
+    # N1 =1/1+e**(somatoria_N1_N2 + bias[0][1])
+    saida_N1_N2 = saidaN(somatoria_N1_N2, bias[0])
+
+    #Calculando somatorio neuronio 3 = (N1*peso[1][0]+N2*peso[1][1])
+    somatoria_N3 = [somatori0(norm_x1, norm_x2, saida_N1_N2[i]) for i in range(len(saida_N1_N2[0]))]
+    somatorio_N3 = list(zip(somatoria_N3[0], somatoria_N3[1]))
+    # v_esperado no Neuronio 3 - usando ativação segmoid = 1/1+e**(somatoria_N1_N2 + bias[1][0])
+    n3 = saidaN(somatorio_N3, bias[1])
+    print('#############################')
+    print(f'v_esperado N3{n3}\n')
+
+    # Calcula o erro da v_esperado calculada
+    erro = [norm_v_esperado[x]-n3[x][0] for x in range(len(v_esperado))]
+    print(f'erro = {erro}\n')
+    print(f'v_esperado esperada{norm_v_esperado}\n\n')
+    print('#############################')
+
+    # Tentar usar  =  (SUM erros)**2
+
+    erroP8 = [saida_N1_N2[x][0]*erro[x] for x in range(len(erro))]
+    erroP9 = [saida_N1_N2[x][1]*erro[x] for x in range(len(erro))]
+
+    # novos pesos
+    for j in range(2):
+        for i in range(len(x1)):
+            pesos[j][0] = pesos[j][0] + tx_aprend * erroP8[i] * norm_x1[i] * (n3[i][0]*(1-n3[i][0]))
+
+    for j in range(2):
+        for i in range(len(x1)):
+            pesos[j][1] = pesos[j][1] + tx_aprend * erroP9[i] * norm_x2[i] * (n3[i][0]*(1-n3[i][0]))
+            
+
+
