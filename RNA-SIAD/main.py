6af202a088c4e3d6a585315b74836694e6b9f912
Hebert Campos <hebertcdev@gmail.com>
2022-06-23 17:08:46 -0300

    Melhorando os comentarios

--- RNA-SIAD/main.py
+++ RNA-SIAD/main.py
@@ -10,27 +10,31 @@ v_esperado = [300000, 545244,45000,5200000,35000,1550000,395000]
 epocas = 10000
 tx_aprend = 0.005
 
-# normaliza entradas entre 0-1
+# normalizando as entradas entre 0-1
 norm_x1 = [(x-min(x1))/(max(x1)-min(x1)) for x in x1]
 norm_x2 = [(x-min(x2))/(max(x2)-min(x2)) for x in x2]
 norm_v_esperado = [(x-min(v_esperado))/(max(v_esperado)-min(v_esperado)) for x in v_esperado]
 
-pesos = peso()
-bias = biass()
+# gerando aleatoriamento os primeiros pesos e bias
+pesos = peso()  # peso = [[x1n1.x1n2],[x2n1,x2n2],[n1n3,n2n3]]
+bias = biass()  # bias = [[n1,n2],[n3]]
 
+# entrando no loop com quantidade de epocas
 for q in range(epocas):
 
     # calcula o somatorio = (x1*peso[0][0]+x2*peso[0][1]+...+xn*peso[0][n])
-    somatoria = [somatori0(norm_x1, norm_x2, pesos[j]) for j in range(2)]
-    somatoria_N1_N2 = list(zip(somatoria[0], somatoria[1]))
+    somatoria = [somatori0(norm_x1, norm_x2, pesos[j]) for j in range(2)]   # esta gerando matriz [[p/n1],[p/n2]]
+    somatoria_N1_N2 = list(zip(somatoria[0], somatoria[1]))     # juntando a matriz
+    
     # v_esperado do Neuronio 1 e 2 - usando ativação segmoid
     # N1 =1/1+e**(somatoria_N1_N2 + bias[0][0])
-    # N1 =1/1+e**(somatoria_N1_N2 + bias[0][1])
+    # N2 =1/1+e**(somatoria_N1_N2 + bias[0][1])
     saida_N1_N2 = saidaN(somatoria_N1_N2, bias[0])
 
     #Calculando somatorio neuronio 3 = (N1*peso[1][0]+N2*peso[1][1])
-    somatoria_N3 = [somatori0(norm_x1, norm_x2, saida_N1_N2[i]) for i in range(len(saida_N1_N2[0]))]
-    somatorio_N3 = list(zip(somatoria_N3[0], somatoria_N3[1]))
+    somatoria_N3 = [somatori0(norm_x1, norm_x2, saida_N1_N2[i]) for i in range(len(saida_N1_N2[0]))]    # esta gerando matriz [[p8],[p9]]
+    somatorio_N3 = list(zip(somatoria_N3[0], somatoria_N3[1]))      # juntando a matriz
+    
     # v_esperado no Neuronio 3 - usando ativação segmoid = 1/1+e**(somatoria_N1_N2 + bias[1][0])
     n3 = saidaN(somatorio_N3, bias[1])
     print('#############################')
